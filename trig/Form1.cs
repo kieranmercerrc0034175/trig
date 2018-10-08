cc1bde74845dd40e6b52df38f0dc3db6e2ac30c2
kiz la <rc0034175@CRO-24-05>
2018-10-08 15:51:54 +0100

    added the graph and labeled it

--- trig/Form1.cs
+++ trig/Form1.cs
@@ -12,9 +12,37 @@ namespace trig
 {
     public partial class Form1 : Form
     {
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double Asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+
+        double
+        {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
